c6d8800003448260aa0807bff311dbd3fcc4ba3e
mavyfaby <maverickfabroa@gmail.com>
2024-01-30 13:41:16 +0800

    feat(app): add initial gestx model

---
+++ model/gestx.py
@@ -0,0 +1,14 @@
+from torch import nn
+
+
+# Behold... The GestX Model
+class GestXModel(nn.Module):
+    def __init__(self):
+        super().__init__(self)
+
+    def forward(self):
+        pass
+
+
+# Instantiate the Model
+model = GestXModel()
